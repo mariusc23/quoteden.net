f633a0f4a1a05501aac274e738cace8f99dc0a07
Paul Craciunoiu <paul.craciunoiu@gmail.com>
2010-03-24 12:40:08 -0700

    Fix quote relationship to votes

--- application/classes/model/quote.php
+++ application/classes/model/quote.php
@@ -2,7 +2,7 @@
 class Model_Quote extends ORM {
     protected $_belongs_to = array('author' => array('model' => 'author', 'foreign_key' => 'author_id'));
     protected $_has_many = array('categories' => array('model' => 'category', 'through' => 'quote_category')
-                               , 'votes' => array('model' => 'vote', 'foreign_key' => 'quote_id'));
+                               , 'votes' => array('model' => 'vote', 'through' => 'votes'));
     protected $_has_one = array('voteaverage' => array('model' => 'voteaverage', 'foreign_key' => 'quote_id'));
 
 
